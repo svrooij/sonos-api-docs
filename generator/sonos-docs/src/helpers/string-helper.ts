f9b5fdd9de9ebd53c392249954ddf2b6f6c4a121
Andrew Dye <andrewwdye@gmail.com>
2024-11-28 08:00:24 -0800

    Fix init cap handling

--- generator/sonos-docs/src/helpers/string-helper.ts
+++ generator/sonos-docs/src/helpers/string-helper.ts
@@ -1,9 +1,9 @@
 export default class StringHelper {
   public static initCapsToKebab(input: string): string {
-    return input.replace(/([a-z0-9])([A-Z]+)/g, '$1-$2').toLowerCase();
+    return `${input[0].toLowerCase()}${input.slice(1)}`.replace(/([a-z0-9])([A-Z]+)/g, '$1-$2').toLowerCase();
   }
 
   public static initCapsToSnake(input: string): string {
-    return input.replace(/([a-z0-9])([A-Z]+)/g, '$1_$2').toLowerCase();
+    return `${input[0].toLowerCase()}${input.slice(1)}`.replace(/([a-z0-9])([A-Z]+)/g, '$1_$2').toLowerCase();
   }
 }
